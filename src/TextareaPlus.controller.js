88b425b7f10171f62ccabeaae58e772a3073b7fa
Chris Pascual <chrisadi.pascual@gmail.com>
2016-09-29 00:02:00 +0800

    Initial Commit

---
+++ src/TextareaPlus.controller.js
@@ -0,0 +1,9 @@
+﻿angular.module("umbraco").controller("TextareaPlus.controller",
+    function ($scope, $http, assetsService) {
+        if ($scope.model.config.trows === null || $scope.model.config.trows === "") {
+            $scope.model.config.trows = 5;
+        }
+        if ($scope.model.config.tcols === null || $scope.model.config.tcols === "") {
+            $scope.model.config.tcols = 500;
+        }
+});
