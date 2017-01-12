d4c8a2b9a95b851a7cb4ab585820794a4d78ad61
Chris Pascual <chrisadi.pascual@gmail.com>
2017-01-12 22:02:36 +0800

    Added auto growth for height property

--- src/TextareaPlus.controller.js
+++ src/TextareaPlus.controller.js
@@ -1,9 +1,8 @@
-﻿angular.module("umbraco").controller("TextareaPlus.controller",
-    function ($scope, $http, assetsService) {
-        if ($scope.model.config.trows === null || $scope.model.config.trows === "") {
-            $scope.model.config.trows = 5;
-        }
-        if ($scope.model.config.tcols === null || $scope.model.config.tcols === "") {
-            $scope.model.config.tcols = 66.6;
-        }
+﻿angular.module("umbraco").controller("TextareaPlus.controller",function ($scope, $http, assetsService) {
+    if ($scope.model.config.trows === null || $scope.model.config.trows === "") {
+        $scope.model.config.trows = 3;
+    }
+    if ($scope.model.config.tcols === null || $scope.model.config.tcols === "") {
+        $scope.model.config.tcols = 66.6;
+    }
 });
